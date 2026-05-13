c335ac676df456c7f2fe000f24f9be579355dbda
trifancristi19 <cristian.trifan@student.nhlstenden.com>
2026-05-13 15:01:32 +0200

    fix: resolve Detekt warnings - newline at EOF and wildcard import

--- app/src/test/java/com/bayg/ExampleUnitTest.kt
+++ app/src/test/java/com/bayg/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.bayg
 
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +13,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-}
+}
